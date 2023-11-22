85225f5dc3afc1621fe8a29821849d19507be83f
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-11-22 13:12:03 +0100

    add stats to details component

--- Travel-List/src/Custom/Details.jsx
+++ Travel-List/src/Custom/Details.jsx
@@ -1,12 +1,15 @@
 const Details = ({ items }) => {
   const numberOfItems = items.length;
   const numberOfPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numberOfPacked / numberOfItems) * 100);
 
   return (
     <footer className="stats">
       <em>
-        You have {numberOfItems} item(s) on your list, and already packed{" "}
-        {numberOfPacked} item(s).
+        {percentage === 100
+          ? "You got everything packed"
+          : ` You have ${numberOfItems} item(s) on your list, and already packed${" "}
+        ${numberOfPacked} item(s) ${percentage} %.`}
       </em>
     </footer>
   );
